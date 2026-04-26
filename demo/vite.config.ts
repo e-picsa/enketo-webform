e29fa151d641f398d88adbd4c4eb790dfe01541b
chrismclarke <chris.m.clarke@live.co.uk>
2026-04-26 16:46:03 -0700

    chore: tidying

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [preact()],
   resolve: {
     alias: {
-      '@picsa/enketo-webform': resolve(__dirname, '../packages/enketo-webform/src/enketo-webform.ts'),
+      "@picsa/enketo-webform/styles": resolve(
+        __dirname,
+        "../packages/enketo-webform/src/styles",
+      ),
+      "@picsa/enketo-webform": resolve(
+        __dirname,
+        "../packages/enketo-webform/src/enketo-webform.ts",
+      ),
     },
   },
-})
+});
